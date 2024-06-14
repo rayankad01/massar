05f776624d2b82c20fec3c1dc708192024b4a4a9
rayankad01 <rayan@RAYANs-MacBook-Air.local>
2024-06-14 12:30:30 +0100

    modification du modeèle User de base et ajout du massarID

--- massar/settings.py
+++ massar/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auths'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'auths.User'
